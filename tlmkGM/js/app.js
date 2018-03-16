b51bcbee5e9c9889a22ed7fa2ad04e6bbf306973
smedranoIds <smedrano.ids@gmail.com>
2018-03-16 10:33:17 -0600

    prueba

---
+++ tlmkGM/js/app.js
@@ -0,0 +1,9 @@
+(function(){
+	var app = angular.module('GM', []);
+
+	app.controller('paso1', ['$scope', function($scope){
+		$scope.plan = ["Individual", "Conyugal", "Familiar"]
+		
+	}])
+
+})();
